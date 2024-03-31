ff7f2b209b3f74d1c906363c4b98e541707d98fd
mike <michaeljyeates@gmail.com>
2024-03-31 22:49:50 +0700

    Add custom signer

--- tools/helpers/distribute_helper.js
+++ tools/helpers/distribute_helper.js
@@ -2,16 +2,22 @@ import {distribute_contract, provider} from "../../lib/contracts.js";
 import {ACCOUNT_COUNT} from "../../lib/env.js";
 
 const DISTRIBUTE_BATCH = process.env.DISTRIBUTE_BATCH || 100;
-export const distributeAll = async () => {
+export const distributeAll = async (customSigner = null) => {
   const iterations = Math.ceil(ACCOUNT_COUNT / DISTRIBUTE_BATCH)
   for (let i = 0; i < iterations; i++) {
     try {
-      console.log(`Distributing from ${i*DISTRIBUTE_BATCH}`);
-      const distribute = await distribute_contract.distribute(i*DISTRIBUTE_BATCH, DISTRIBUTE_BATCH)
+      console.log(`Distributing from ${i*DISTRIBUTE_BATCH} on contract ${process.env.DISTRIBUTE_CONTRACT}`);
+      let distribute;
+      if (customSigner){
+        distribute = await distribute_contract.connect(customSigner).distribute(i*DISTRIBUTE_BATCH, DISTRIBUTE_BATCH)
+      }
+      else {
+        distribute = await distribute_contract.distribute(i*DISTRIBUTE_BATCH, DISTRIBUTE_BATCH)
+      }
       await provider.waitForTransaction(distribute.hash)
       console.log(`Distributed in tx ${distribute.hash}`)
     } catch (e) {
-      throw new Error(`Error in final step: ${e}. Iteration: ${i}`)
+      throw new Error(`Error in distribute step: ${e}. Iteration: ${i}`)
     }
   }
 }
