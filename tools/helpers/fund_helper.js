ec2eadde2ecdd92fe4f76ab6e1d9a33e398c7bf4
mike <michaeljyeates@gmail.com>
2024-03-31 20:16:22 +0700

    Tidy up

--- tools/helpers/fund_helper.js
+++ tools/helpers/fund_helper.js
@@ -5,18 +5,22 @@ export const approveAndFund = async (customSigner = null) => {
   const uc = (customSigner) ? usdb_contract.connect(customSigner) : usdb_contract;
   const wc = (customSigner) ? weth_contract.connect(customSigner) : weth_contract;
 
+  const eth_qty = BigNumber.from(process.env.ETH_QUANTITY);
+  const usdb_qty = BigNumber.from(process.env.USDB_QUANTITY);
+  const weth_qty = BigNumber.from(process.env.WETH_QUANTITY);
+
   console.log("Approving and funding")
-  const approveUSDB = await uc.approve(process.env.DISTRIBUTE_CONTRACT, BigNumber.from(process.env.USDB_QUANTITY));
+  const approveUSDB = await uc.approve(process.env.DISTRIBUTE_CONTRACT, usdb_qty);
   console.log(`Approved USDB ${approveUSDB.hash}`)
-  const approveWETH = await wc.approve(process.env.DISTRIBUTE_CONTRACT, BigNumber.from(process.env.WETH_QUANTITY));
+  const approveWETH = await wc.approve(process.env.DISTRIBUTE_CONTRACT, weth_qty);
   console.log(`Approved WETH ${approveWETH.hash}`)
 
   let sendFunds;
   if (customSigner){
-    sendFunds = await distribute_contract.connect(customSigner).fund({value: process.env.ETH_QUANTITY});
+    sendFunds = await distribute_contract.connect(customSigner).fund({value: eth_qty});
   }
   else {
-    sendFunds = await distribute_contract.fund({value: process.env.ETH_QUANTITY});
+    sendFunds = await distribute_contract.fund({value: eth_qty});
   }
   console.log(`Funds sent to contract ${sendFunds.hash}`)
 }
