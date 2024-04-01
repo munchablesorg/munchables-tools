549855614eaf7f744ba8fdfe7a8dfefbc69e5620
mike <michaeljyeates@gmail.com>
2024-04-01 21:09:28 +0700

    Fix distributor

--- tools/stages/4-distribute.js
+++ tools/stages/4-distribute.js
@@ -1,6 +1,9 @@
 import {distributeAll} from "../helpers/distribute_helper.js";
+import {provider} from "../../lib/contracts.js";
+import {approveAndFund} from "../helpers/fund_helper.js";
 
 (async () => {
     console.log(`Distributing funds`);
-    await distributeAll();
+    const distributor = await provider.getSigner(process.env.DISTRIBUTE_CONTRACT_OWNER);
+    await distributeAll(distributor);
 })();
