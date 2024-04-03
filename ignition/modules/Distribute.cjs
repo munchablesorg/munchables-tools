065dd7b18b56be3be0d4eb6d49bac2776bbf9b86
0xinsanity <noahsahar@gmail.com>
2024-04-03 16:33:03 -0400

    added account count caveat

--- ignition/modules/Distribute.cjs
+++ ignition/modules/Distribute.cjs
@@ -1,11 +1,12 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("DistributeModule", (m) => {
+  const identifier = process.env.DEPLOY_IDENTIFIER || "0";
   const dist = m.contract(
       "Distribute",
       [process.env.USDB_CONTRACT, process.env.WETH_CONTRACT,
             process.env.ETH_QUANTITY, process.env.USDB_QUANTITY, process.env.WETH_QUANTITY,
             process.env.LOCK_CONTRACT]
   );
-  return { dist };
+  return { [`dist_${identifier}`]: dist };
 });
