e2ac9feeed80521e71f0f8244b4dd07157a461cd
0xinsanity <noahsahar@gmail.com>
2024-04-07 16:54:44 -0400

    fixed fund operation

--- tools/stages/3-fund.js
+++ tools/stages/3-fund.js
@@ -1,9 +1,16 @@
 import {approveAndFund} from "../helpers/fund_helper.js";
 import {provider} from "../../lib/contracts.js";
-
+import {ethers} from "ethers";
 (async () => {
     // NOTE: This stage is only to be run if using local clone of mainnet
     // This stage should be run via the multisig in practice
+    await provider.send('hardhat_impersonateAccount', [process.env.MSIG]);
+    let currentBalance = await provider.getBalance(process.env.MSIG);
+    currentBalance = BigInt(currentBalance.toString());
+    const increaseAmount = BigInt(1e17); 
+    const newBalance = currentBalance + increaseAmount;
+    // Update the balance
+    await provider.send('hardhat_setBalance', [process.env.MSIG, `0x${newBalance.toString(16)}`]);
     const msigOwner = await provider.getSigner(process.env.MSIG);
     await approveAndFund(process.env.DISTRIBUTE_CONTRACT_OWNER, msigOwner);
 })();
